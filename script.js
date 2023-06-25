81996babe1e60bf1d00dd9c81ad153819eb1a330
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-06-26 00:24:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function delayHello(){
+	return new Promise((resolve)=>{
+		setTimeout(() => {
+			resolve("Hello, world!")
+		},1000);
+	});
+} 
+//usage of Promise
+ const updateContent = document.getElementById("output");
+
+delayHello().then((message) => {
+	updateContent.textContent = message;
+}).catch((error) => {
+	console.error("An error occurred:", error);
+})
